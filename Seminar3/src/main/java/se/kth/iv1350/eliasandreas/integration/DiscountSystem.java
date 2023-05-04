e07fe0d6e6c2a286318f7923d7d33746c58b4c5f
Morgochov <124572863+Morgochov@users.noreply.github.com>
2023-05-04 19:56:40 +0200

    Delete DiscountSystem.java
    not needed

--- Seminar3/src/main/java/se/kth/iv1350/eliasandreas/integration/DiscountSystem.java
+++
@@ -1,8 +0,0 @@
-package main.java.se.kth.iv1350.eliasandreas.integration;
-
-public class DiscountSystem {
-    
-    public DiscountDTO searchDiscount(String customerID){
-        return new DiscountDTO();
-    }
-}
